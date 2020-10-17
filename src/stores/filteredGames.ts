10fe42200cd232cd3f540fc207efab3710c437a8
Michael Speed Elder <michael@planoly.com>
2020-10-17 11:59:20 -0500

    alphabetize by default

--- src/stores/filteredGames.ts
+++ src/stores/filteredGames.ts
@@ -2,6 +2,8 @@ import type { Game } from '../types/Game';
 import gamesJSON from './data/games';
 import { writable } from 'svelte/store';
 
-const filteredGames = writable<Game[]>(gamesJSON);
+const filteredGames = writable<Game[]>(
+  gamesJSON.sort((a, b) => (a.title > b.title ? 1 : -1))
+);
 
 export default filteredGames;
